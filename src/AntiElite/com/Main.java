dee56a9a883774cb03c6728e02a4d47cb79535e7
Nametsego <Nametsegothakadu@gmail.com>
2022-01-04 06:14:18 +0200

    Initial Commit

---
+++ src/AntiElite/com/Main.java
@@ -0,0 +1,10 @@
+package AntiElite.com;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Bro, do you even code?","Message", JOptionPane.YES_NO_CANCEL_OPTION);
+    }
+}
